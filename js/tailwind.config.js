e5526b20f80a402d7390d7af6f77093e1bca8c7a
theniloywebdev <husnemobarakniloy2001@gmail.com>
2023-09-29 21:22:22 +0600

    class_27

---
+++ js/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        // MY CUSTOM COLOR START 
+        colors: {
+            niloyColor:"#E55604",
+        },
+        // MY CUSTOM COLOR END
+
+        height:{
+            niloyHeight:"800px",
+        }, 
+
+
+        backgroundImage: {
+        bannerImage:"url('images/nature.png')"
+        },
+
+    }
+    }
+  }
